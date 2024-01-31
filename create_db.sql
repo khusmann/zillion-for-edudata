66b5879ce2309ac3d70486bdbac1c59e29b9dfc7
Kyle Husmann <kyle.husmann@gmail.com>
2024-01-30 18:58:14 -0800

    add teacher queries

--- create_db.sql
+++ create_db.sql
@@ -34,6 +34,15 @@ display_name TEXT
 
 .import --skip 1 data/waves.csv waves
 
+CREATE TABLE weeks (
+id_wave INT NOT NULL,
+week_num INT,
+id_week INT PRIMARY KEY,
+display_name TEXT
+);
+
+.import --skip 1 data/weeks.csv weeks
+
 CREATE TABLE student_scores (
 id_student INT NOT NULL,
 id_wave INT NOT NULL,
@@ -45,3 +54,33 @@ PRIMARY KEY (id_student, id_wave)
 );
 
 .import --skip 1 data/student_scores.csv student_scores
+
+CREATE TABLE teacher_weekly (
+id_teacher INT NOT NULL,
+id_week INT NOT NULL,
+teacher_stressLevel INT,
+PRIMARY KEY (id_teacher, id_week)
+);
+
+.import --skip 1 data/teacher_weekly.csv teacher_weekly
+
+CREATE VIEW teacher_weekly_avg AS
+SELECT id_teacher, AVG(teacher_stressLevel) as teacher_stressLevel_avg from teacher_weekly GROUP BY id_teacher;
+
+CREATE TABLE teachers (
+id_teacher INT PRIMARY KEY,
+id_school INT NOT NULL,
+teacher_name TEXT
+);
+
+.import --skip 1 data/teachers.csv teachers
+
+CREATE TABLE teacher_student_ratings (
+id_student INT NOT NULL,
+id_wave INT NOT NULL,
+id_teacher INT NOT NULL,
+student_socialskill INT,
+student_motivation INT
+);
+
+.import --skip 1 data/teacher_student_ratings.csv teacher_student_ratings
--- run_query.py
+++ run_query.py
@@ -10,3 +10,28 @@ result = wh.execute(
 )
 
 print(result.df)
+
+
+result = wh.execute(
+    metrics=["teacher_stressLevel"],
+    dimensions=["school_type"],
+)
+
+print(result.df)
+
+result = wh.execute(
+    metrics=["student_socialskill"],
+    dimensions=["school_type"],
+)
+
+print(result.df)
+
+result = wh.execute(
+    metrics=["student_socialskill"],
+    dimensions=[
+        "id_wave", "id_teacher", "id_student",
+        "school_type", "teacher_stressLevel_avg",
+    ],
+)
+
+print(result.df)
